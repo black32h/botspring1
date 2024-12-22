72755e627e54669f334d0bf3af1752b3f493f847
black32h <blackspetya@gmail.com>
2024-12-22 15:23:56 +0200

    Update BotConfig.java

--- src/main/java/org/example/bot/BotConfig.java
+++ src/main/java/org/example/bot/BotConfig.java
@@ -1,4 +1,29 @@
 package org.example.bot;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BotConfig {
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    // Геттер для botUsername
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    // Геттер для botToken
+    public String getBotToken() {
+        return botToken;
+    }
 }
+
