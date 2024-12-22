5f0903fb12906e28d8588637adbcfc9bf995905a
black32h <blackspetya@gmail.com>
2024-12-22 15:25:25 +0200

    Update LaunchPoolInfo.java

--- src/main/java/org/example/model/LaunchPoolInfo.java
+++ src/main/java/org/example/model/LaunchPoolInfo.java
@@ -1,4 +1,23 @@
 package org.example.model;
 
 public class LaunchPoolInfo {
+    private String exchange;
+    private String launchPool;
+    private String pools;
+    private String period;
+    private String status;
+    public LaunchPoolInfo(String exchange, String launchPool, String pools, String period, LaunchPoolReader.PoolStatus status) {
+        this.exchange = exchange;
+        this.launchPool = launchPool;
+        this.pools = pools;
+        this.period = period;
+        this.status = String.valueOf(status);
+
+    }
+
+
+    public String getLaunchPool() {
+        return launchPool;
+    }
 }
+
